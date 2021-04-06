1afbd8d2006cda2a83ad131eaefe7cf078a94de0
Entreprenerd <valeranijr@gmail.com>
2021-04-06 17:27:44 +0200

    feat: once per hour check for sale and auction end event

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const axios = require('axios')
 /**
  * Cron config that gives you an opportunity
  * to run scheduled jobs.
@@ -10,12 +10,69 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/concepts/configurations.html#cron-tasks
  */
 
+/**
+ * Check if asset has sold, for the api, if a last_sale exists, then the item was sold
+ * @returns boolean
+ */
+const hasSold = (asset) => {
+  return !!asset.last_sale
+}
+
+/**
+ * Given a asset that sold return total price
+ * @param {*} asset 
+ * @returns 
+ */
+const soldFor = asset => {
+  try {
+    return asset.last_sale.total_price
+  } catch(err){
+    return ""
+  }
+}
+/**
+ * 
+ * @param {*} asset 
+ */
+const auctionEnded = asset => {
+  const salesOrder = asset.orders.find(order => String(order.side) === "1")
+  if(!salesOrder){
+    return false
+  }
+
+  return salesOrder.listing_time * 1000 > new Date().getTime();
+}
 module.exports = {
   /**
-   * Simple example.
-   * Every monday at 1am.
+   * Once per hour, check for sales and end of auction events
    */
-  // '0 1 * * 1': () => {
-  //
-  // }
+  '0 0 * * * *': async () => {
+    // Get all asset that are currently on sale
+    const allAssets = await strapi.services.token.find({_limit: -1, onSale: true, sold: false})
+    console.log("allAssets", allAssets.length)
+
+    const res = await Promise.all(allAssets.map(async(asset) => {
+      try{
+        const singleAssetCheck = await axios({
+          method: "GET",
+          url: `https://api.opensea.io/api/v1/asset/${asset.address}/${asset.tokenId}`,
+          headers: {
+            "X-API-KEY": process.env.OPENSEA_API_KEY || undefined
+          }
+        })
+        const result = singleAssetCheck.data
+        if(auctionEnded(result) || hasSold(result)){
+          await strapi.services.token.update({id: asset.id}, {
+            onSale: !auctionEnded(result),
+            sold: hasSold(result),
+            soldFor: soldFor(result)
+          })
+        }
+      } catch(err){
+        console.log("Exception in updating an asset", err)
+      }
+    }))
+
+    // Check if it has sold
+  }
 };
