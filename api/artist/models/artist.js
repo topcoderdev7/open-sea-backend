8eb9bbfc5c6c933193ebff5c4d9d4e55c1eaf49f
noman <topcoderdev7@gmail.com>
2021-06-02 02:22:50 -0700

    verify icon and gltf link

--- api/artist/models/artist.js
+++ api/artist/models/artist.js
@@ -4,5 +4,5 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
-
+                
 module.exports = {};
